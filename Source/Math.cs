9192b12cae9e6fb8dc5717ce2d0f57d1fc548301
Thalita Mariano <tmariano@eusou.space>
2020-05-20 08:28:26 -0300

    adding challenge 01, Fibonnaci Sequence

---
+++ Source/Math.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Codenation.Challenge
+{
+    public class Math
+    {
+        public List<int> Fibonacci()
+        {
+            int n1 = 1, n2 = 0, sum = 0;
+
+            List<int> sequence = new List<int>();
+
+            sequence.Add(n2);
+            while (sum <= 144)
+            {
+                sum = n1 + n2;  
+                n1 = n2;        
+                n2 = sum;
+                sequence.Add(sum);
+            }
+
+            return sequence;
+        }
+
+        public bool IsFibonacci(int numberToTest)
+        {
+            bool isFibonacci = false;
+
+            List<int> sequence = Fibonacci();
+
+            foreach(int number in sequence)
+            {
+                if(number == numberToTest)
+                {
+                    isFibonacci = true;
+                }
+            }
+
+
+            return isFibonacci;
+        }
+    }
+}
